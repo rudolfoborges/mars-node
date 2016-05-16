9aa89e3313d4ed050c772a2d68512602dd686c2a
Rudolfo Borges <oliveira.rudolfo@gmail.com>
2016-05-16 02:34:51 -0300

    Primeiro commit

--- direcao.js
+++ direcao.js
@@ -5,7 +5,7 @@
         NORTE: {A: 'NORTE', E: 'OESTE', D: 'LESTE', M: {x: 0, y: 1}},
         LESTE: {A: 'LESTE', E: 'NORTE', D: 'SUL', M: {x: 1, y: 0}},
         SUL: {A: 'SUL', E: 'LESTE', D: 'OESTE', M: {x: 0, y: -1}},
-        OESTE: {A: 'OESTE', E: 'SUL', D: 'NORTE', M: {x: -1, y: -0}}
+        OESTE: {A: 'OESTE', E: 'SUL', D: 'NORTE', M: {x: -1, y: 0}}
     };
 
     module.exports = DIRECAO;
