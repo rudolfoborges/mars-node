bafdcdc44140680026c670fa32c398f2db8eec3d
Rudolfo Borges <oliveira.rudolfo@gmail.com>
2016-05-16 07:25:55 -0300

    Alterações no modelo Sonda

--- app.js
+++ app.js
@@ -3,19 +3,18 @@
 let nasa = require('./models/nasa');
 let Planalto = require('./models/planalto');
 let Posicao = require('./models/posicao');
-let DIRECAO = require('./models/direcao');
+let Direcao = require('./models/direcao');
 
 
 let planalto = new Planalto(10, 10);
 let posicao = new Posicao(0,0);
-let NORTE = DIRECAO.NORTE;
 
-let sonda1 = nasa.lancamento(planalto, posicao, NORTE);
+let sonda1 = nasa.lancamento(planalto, posicao, Direcao.NORTE);
 nasa.enviarComando(sonda1, ['M', 'M', 'M', 'E', 'D', 'E', 'E', 'M']);
 nasa.informarPosicao(sonda1);
 
-let sonda2 = nasa.lancamento(planalto, posicao, NORTE);
+let sonda2 = nasa.lancamento(planalto, posicao, Direcao.NORTE);
 nasa.informarPosicao(sonda2);
 
-let sonda3 = nasa.lancamento(planalto, posicao, NORTE);
+let sonda3 = nasa.lancamento(planalto, posicao, Direcao.NORTE);
 nasa.informarPosicao(sonda3);
